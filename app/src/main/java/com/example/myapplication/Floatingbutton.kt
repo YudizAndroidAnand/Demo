75db766568bfa9ff6bea6f1161921e317f8fe370
yudizsolutionsltd <anand.patel@yudiz.com>
2023-05-02 13:13:26 +0530

    Drawables-snackbar for floating button

---
+++ app/src/main/java/com/example/myapplication/Floatingbutton.kt
@@ -0,0 +1,36 @@
+package com.example.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import com.google.android.material.floatingactionbutton.ExtendedFloatingActionButton
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.snackbar.Snackbar
+
+class Floatingbutton : AppCompatActivity() {
+
+    lateinit var   floatbtn : FloatingActionButton
+    lateinit var extfloatbnt : ExtendedFloatingActionButton
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_floatingbutton)
+
+        floatbtn = findViewById(R.id.floatingbtn)
+        extfloatbnt = findViewById(R.id.exfloatingbtn)
+
+        floatbtn.setOnClickListener{
+            Snackbar.make(it, "Simple Snackbar", Snackbar.LENGTH_LONG).show()
+        }
+
+
+        extfloatbnt.setOnClickListener{
+
+            Snackbar.make(it, "Another Snackbar", Snackbar.LENGTH_LONG).setAction("view") {
+                Snackbar.make(it, "Welcomes you!!!", Snackbar.LENGTH_LONG).show()
+            }.show()
+
+        }
+
+    }
+}
